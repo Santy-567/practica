19376ba3b2ce0bd8f58fdce1a821d4857bcd702c
Santy-567 <vera.santiago@correounivalle.edu.co>
2025-07-20 08:55:05 -0500

    Boss

--- Enemigos/boss_1.gd
+++ Enemigos/boss_1.gd
@@ -3,12 +3,16 @@ extends Node2D
 
 @onready var AniBoss = $AnimationPlayer
 const LanCARACOL = preload("res://Enemigos/Caracol.tscn")
-
-func _process(delta):
-	AniBoss.play("Lanza")
 	
 func LanzarCaracol():
 	var InstCarac = LanCARACOL.instantiate()
 	InstCarac.position = $SlimeBlockJump/Mano/SnailShell.position
 	
 	add_child(InstCarac)
+	
+	AniBoss.play("Reposo")
+
+
+func _on_timer_timeout():
+	AniBoss.play("Lanza")
+	$Timer.wait_time = randf_range(2,5)
