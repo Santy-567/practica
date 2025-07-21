426d84a8843bed2d15f344cb89dc74d00c129496
Santy-567 <vera.santiago@correounivalle.edu.co>
2025-07-21 08:57:55 -0500

    Jugador

--- Personajes/yellow.gd
+++ Personajes/yellow.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var gravity = 250
 @export var jump = 250
 @export var speed = 100
+@onready var AniPlayer: AnimationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 	
@@ -17,6 +18,14 @@ func _physics_process(delta):
 	var Movi = Input.get_axis("Izquierda","Derecha")
 	velocity.x = speed * Movi
 	
+	if Movi == 0:
+		AniPlayer.play("Quieto")
+		
+	elif  Movi > 0:
+		AniPlayer.play("Adelante")
+		
+	else:
+		AniPlayer.play("Atras")
 	#Gravedad
 	if not is_on_floor():
 		velocity.y = velocity.y + gravity * delta
